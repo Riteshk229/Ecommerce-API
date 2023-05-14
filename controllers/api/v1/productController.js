0aa34b2efdcc0719d5a7a7d4c10a32a28528dc7a
Ritesh <Riteshk229@gmail.com>
2023-05-14 16:38:03 +0530

    added function to delete and update a particular product

--- controllers/api/v1/productController.js
+++ controllers/api/v1/productController.js
@@ -1,21 +1,22 @@
 const Product = require('../../../models/products');
 
 module.exports.listProducts =  async function(req,res){
-
+    
     try{
         let product = await Product.find({});
-
         const formattedProducts = product.map((product,index) => ({
-            id: index + 1,
+            id: product.id,
             name: product.name,
             quantity: product.quantity
-          }));
-
+        }));
+        
         return res.status(200).json({
-            data : formattedProducts,
+            data : {
+                Products : formattedProducts,
+            },
             message: "These are the Products in Inventory"
         });
-
+        
     } catch (err){
         console.log("****",err);
         return res.status(500).json({
@@ -28,7 +29,7 @@ module.exports.listProducts =  async function(req,res){
 module.exports.create = async function(req,res){
     // console.log(req.body.name, req.body.quantity);
     try{
-
+        
         let newProduct = await Product.create({
             name:req.body.name,
             quantity:req.body.quantity
@@ -36,7 +37,12 @@ module.exports.create = async function(req,res){
         
         await newProduct.save();
         return res.status(200).json({
-            data : newProduct,
+            data : {
+                Product : {
+                    name : newProduct.name,
+                    quantity : newProduct.quantity
+                }
+            },
             message : "Product Added!!"
         });
         
@@ -44,6 +50,77 @@ module.exports.create = async function(req,res){
         console.log("****",err);
         return res.status(500).json({
             message : "Error in adding product to inventory"
+        });
+    }
+};
+
+module.exports.delete = async function(req,res){
+    console.log(req.params.id);
+    try{
+
+        const id = req.params.id;
+        let product = await Product.findById(id);
+        if(!product){
+            return res.status(404).json({
+                message: "Product not found!!"
+            });
+        }
+        
+        let deleteProduct = await product.deleteOne();
+
+        return res.status(200).json({
+            data : {
+                product : {
+                    id : deleteProduct.id,
+                    name: deleteProduct.name,
+                    quantity: deleteProduct.quantity
+                }
+            },
+            message : "Product deleted successfully"
+        });
+        
+    }catch(err){
+        console.log("****",err);
+        return res.status(500).json({
+            message : "Error in deleting product"
+        });
+    }
+};
+
+module.exports.updateQuantity = async function(req,res){
+
+    try{
+        
+        const update_quantity = req.query.number;
+        const id = req.params.id;
+        
+        let product = await Product.findById(id);
+        
+        if(!product){
+            return res.status(404).json({
+                message: "Product not found!!"
+            });
+        }
+        
+        product.quantity += parseInt(update_quantity,10);
+        
+        let updatedProduct = await product.save();
+        
+        return res.status(200).json({
+            data : {
+                product : {
+                    id : updatedProduct.id,
+                    name: updatedProduct.name,
+                    quantity: updatedProduct.quantity
+                }
+            },
+            message : "Product updated successfully"
+        });
+        
+    }catch(err){
+        console.log("****",err);
+        return res.status(500).json({
+            message : "Error in updating quantity"
         });
     }
 };
--- routes/api/v1/product_api.js
+++ routes/api/v1/product_api.js
@@ -6,5 +6,7 @@ const productController = require('../../../controllers/api/v1/productController
 
 router.get('/',productController.listProducts);
 router.post('/create',productController.create);
+router.delete('/:id',productController.delete);
+router.post('/:id/update_quantity',productController.updateQuantity);
 
 module.exports = router;
