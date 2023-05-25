a01b98378f519de19e37fd03693db85ce4f5b145
Ritesh <100128015+Riteshk229@users.noreply.github.com>
2023-05-25 18:45:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const db = require('./config/mongoose');
 
 // Creating an App with Express
 const app = express();
-const port = 3000;
+const port = process.env.port || 3000;
 
 //Enable us to pass data through URL
 app.use(express.urlencoded({extended:true}));
