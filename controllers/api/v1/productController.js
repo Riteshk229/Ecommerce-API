a584b022a68c5d5d3ddf858bc2515db1964ea224
Ritesh <Riteshk229@gmail.com>
2023-05-25 19:06:58 +0530

    done

--- controllers/api/v1/productController.js
+++ controllers/api/v1/productController.js
@@ -177,3 +177,4 @@ module.exports.updateQuantity = async function(req,res){
         });
     }
 };
+
--- index.js
+++ index.js
@@ -9,27 +9,13 @@ const expressLayout = require('express-ejs-layouts');
 const app = express();
 const port = process.env.port || 3000;
 
-
-//For static files
-app.use(express.static('./assets'));
-
-
-app.use(expressLayout);
-// Enables the use of external styles and scripts
-app.set('layout extractStyles',true);
-app.set('layout extractScripts',true);
-
-// Setting view Engine
-app.set('view engine','ejs');
-app.set('views','./views')
-
 //Enable us to pass data through URL
 app.use(express.urlencoded({extended:true}));
 
 //Routes
 app.use('/',require('./routes/index'));
 
-app.listen(port,()=>{
+app.listen(port,(err)=>{
     
     if(err){
         console.log("Error in running the server");
