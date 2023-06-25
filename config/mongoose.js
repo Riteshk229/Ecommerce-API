c8bf2cda56115c6d514afa931cec36be7e32ae6c
Ritesh <Riteshk229@gmail.com>
2023-06-25 12:05:14 +0530

    commit

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,11 +2,7 @@
 const mongoose = require('mongoose');
 
 // Connecting the database
-<<<<<<< HEAD
 mongoose.connect("mongodb://0.0.0.0:27017/EcommerceAPI");
-=======
-mongoose.connect("mongodb+srv://Riteshk229:9693640242@cluster0.lucaj3w.mongodb.net/EcomAPI?retryWrites=true&w=majority");
->>>>>>> 8a56a2cbbb3b1c4ed73d6b4b2a70319eebb4640f
 
 const db = mongoose.connection;
 
@@ -19,8 +15,4 @@ db.once('open',function(){
     console.log("*** Connected to Database :: MongoDB ***");
 });
 
-<<<<<<< HEAD
 module.exports = db;
-=======
-module.exports = db;
->>>>>>> 8a56a2cbbb3b1c4ed73d6b4b2a70319eebb4640f
--- index.js
+++ index.js
@@ -2,16 +2,9 @@
 const express = require('express');
 const db = require('./config/mongoose');
 
-<<<<<<< HEAD
 // Creating an App with Express
 const app = express();
 const port = 3000;
-=======
-
-// Creating an App with Express
-const app = express();
-const port = process.env.port || 3000;
->>>>>>> 8a56a2cbbb3b1c4ed73d6b4b2a70319eebb4640f
 
 //Enable us to pass data through URL
 app.use(express.urlencoded({extended:true}));
@@ -19,17 +12,6 @@ app.use(express.urlencoded({extended:true}));
 //Routes
 app.use('/',require('./routes/index'));
 
-<<<<<<< HEAD
 app.listen(port,()=>{
     console.log(`Server up and running on port:  ${port}`);
 });
-=======
-app.listen(port,(err)=>{
-    
-    if(err){
-        console.log("Error in running the server");
-        return;
-    }
-    console.log(`Server up and running on port:  ${port}`);
-});
->>>>>>> 8a56a2cbbb3b1c4ed73d6b4b2a70319eebb4640f
